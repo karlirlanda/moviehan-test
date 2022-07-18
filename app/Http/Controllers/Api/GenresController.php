2eefe7ee86babad67a5080ce73ab1f5c13fbae2e
karlirlanda <karlirlanda@gmail.com>
2022-07-18 11:50:45 +0800

    working crud genres

--- app/Http/Controllers/Api/GenresController.php
+++ app/Http/Controllers/Api/GenresController.php
@@ -17,147 +17,80 @@ class GenresController extends Controller
      */
     public function index(Request $request, Genres $genres)
     {
-        // $genres = Genres::all();
+        $genres = Genres::all();
 
-        // return response()->json($genres, 200);
-
-        dd($genres);
+        return response()->json($genres, 200);
     }
 
-    // /**
-    //  * Store a newly created resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function store(Request $request)
-    // {
-    //     try
-    //     {
-    //         $validateUser = Validator::make($request->all(),
-    //         [
-    //             'name' => 'required',
-    //             'email' => 'required|email|unique:users,email',
-    //             'password' => 'required'
-    //         ]);
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $genres = Genres::create($request->all());
 
-    //         if($validateUser->fails())
-    //          {
-    //              return response()->json([
-    //             'status' => 'false',
-    //             'message' => 'Validation Error',
-    //             'error' => $validateUser->errors()
-    //              ], 401);
-    //          }
+        return response()->json([
+            'status' => true,
+            'message' => "Genre created successfuly",
+            'genres' => $genres
+        ],200);
+    }
 
-    //          $user = User::create([
-    //                 'name' => $request->name,
-    //                 'email' => $request->email,
-    //                 'password' => Hash::make($request->password)
-    //          ]);
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-    //          return response()->json([
-    //             'status' => 'true',
-    //             'message' => 'User created successfully',
-    //             'token' => $user->createToken("API TOKEN")->plainTextToken
-    //              ], 200);
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $genres = Genres::findorFail($id);
+        if($genres)
+        {
+            $genres->genre_name = $request->genre_name;
+            $genres->update();
 
-    //     }
-    //     catch (\Throwable $th)
-    //     {
-    //         return response()->json([
-    //             'status' => 'false',
-    //             'message' => $th->getMessage(),
-    //             'error' => $validateUser->errors()
-    //              ], 500);
-    //     }
-    // }
+            return response()->json([
+                'message'=>'Genres updated succesfuly!',
+                'genres' => $genres
+            ], 200);
+        }
+        else
+        {
+            return response()->json(['message'=>'No Genre found!'], 404);
+        }
+    }
 
-    // /**
-    //  * Display the specified resource.
-    //  *
-    //  * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function show($id)
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Update the specified resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function update(Request $request, $id)
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Remove the specified resource from storage.
-    //  *
-    //  * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function destroy($id)
-    // {
-    //     $user = User::findOrFail($id);
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $genres = Genres::findOrFail($id);
        
-    //     $user->delete();
+        $genres->delete();
 
-    //     return response()->json([
-    //             'message'=>'User deleted succesfuly!',
-    //             'user' => $user
-    //     ], 200);
-    // }
-    // public function login(Request $request)
-    // {
-    //     try
-    //     {
-    //         $validateUser = Validator::make($request->all(),
-    //         [
-                
-    //             'email' => 'required|email',
-    //             'password' => 'required'
-    //         ]);
-    
-    //         if($validateUser->fails())
-    //          {
-    //              return response()->json([
-    //             'status' => 'false',
-    //             'message' => 'Validation Error',
-    //             'error' => $validateUser->errors()
-    //              ], 401);
-    //          }
-    
-    //          if(!Auth::attempt($request->only(['email','password'])))
-    //          {
-    //             return response()->json([
-    //                 'status' => 'false',
-    //                 'message' => 'Email & Password does not match with our record',
-    //                 'error' => $validateUser->errors()
-    //             ], 401);
-    //          }
-             
-    //          $user = User::where('email', $request->email)->first();
-
-    //          return response()->json([
-    //             'status' => 'true',
-    //             'message' => 'Login Successfully',
-    //             'token' => $user->createToken("API TOKEN")->plainTextToken
-    //              ], 200);
-    
-    //     }
-    //     catch (\Throwable $th)
-    //     {
-    //         return response()->json([
-    //             'status' => 'false',
-    //             'message' => $th->getMessage(),
-    //             'error' => $validateUser->errors()
-    //              ], 500);
-    //     }
-    // }
+        return response()->json([
+                'message'=>'Genre deleted succesfuly!',
+                'genres' => $genres
+        ], 200);
+    }
 }
