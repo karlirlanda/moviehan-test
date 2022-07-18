b46fe78c5ff17345ed0bd4537db6a32cdc01250e
karlirlanda <karlirlanda@gmail.com>
2022-07-18 18:54:46 +0800

    reset password no css

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,7 +32,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();      
         
-        if(Auth::user()->role == 1)
+        if(Auth::user()->is_admin == 1)
         {
             return redirect()->intended(RouteServiceProvider::ADMIN);
         }
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,48 +1,79 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900;8..144,1000&family=Roboto:wght@900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{URL::asset('/css/Resetpass.css')}}">
+        <link rel="stylesheet" href="{{URL::asset('/css/queries/login media.css')}}">
+    </head>
+    <body>
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
         <form method="POST" action="{{ route('password.update') }}">
             @csrf
 
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+        <div class="main-bg">
+            <div class="main-body-wrapper">
+                <div class="logo-wrapper">
+                    <a href="{{url('/')}}"><img class="logo" src="{{URL::asset('/pic/last logo.png')}}"></a>
+                </div>
+                <div class="login-content-wrapper">
+                    <!-- Password Reset Token -->
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                    <div class="login-content-wrapper">
+                        <div class="login-content">
+                            <h1>Please input your new password here!</h1>                        
+                        </div>
+                    </div>
+                     <!-- Email Address -->
+                        <div>
+                            <label for="email" :value="__('Email')">
+                                <input id="email" type="email" name="email" :value="old('email', $request->email)" value="Please input your email address here" autofocus />
+                            </label>                           
+                        </div>
+                        <!-- Password -->
+                        <div>
+                            <label for="password" :value="__('Password')">
+                                <input id="password" type="password" name="password" required />
+                            </label>
+                        </div>
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+                        <!-- Confirm Password -->
+                        <div>
+                            <label for="password_confirmation" :value="__('Confirm Password')">
+                            <input id="password_confirmation" 
+                                                type="password"
+                                                name="password_confirmation" required />
+                            </label>
+                        </div>
+                        <div>
+                            <button>
+                                {{ __('Reset Password') }}
+                            </button>
+                        </div>
+                </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
+        
+            <div class="bottom-wrapper">
+                <div class="bottom-div">
+                    <div class="question-nav">
+                        <a>Questions? Contact us</a>
+                    </div>
+                    <div class="navs">
+                        <a>FAQ</a>
+                        <a>Help Center</a>
+                        <a>Terms of Use</a>
+                        <a>Privacy</a>
+                    </div>
+                    <div class="navs">
+                        <a>Cookie Preferences</a>
+                        <a>Corporate Information</a>
+                    </div>
+                </div>
             </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
+        </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </body>
+</html>
